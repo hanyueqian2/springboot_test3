1ff43253add3bdcb56f35cc1b235ced0c5e1a6f4
han <han_hello@163.com>
2018-11-17 15:10:26 +0800

    修改测试代码

---
+++ src/test/java/com/han/test/springboot_test3/Fraction.java
@@ -0,0 +1,18 @@
+package com.han.test.springboot_test3;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Fraction {
+
+    private Fraction c;
+
+    public Fraction getC() {
+        return c;
+    }
+
+    public void setC(Fraction c) {
+        this.c = c;
+    }
+}
+
