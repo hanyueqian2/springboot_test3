1df6bc482b49b9e8735be2f1af4a4d3f06ca47e1
han <han_hello@163.com>
2018-11-20 16:53:18 +0800

    完成MapStruct的test,使其正常运行

--- src/main/java/com/han/test/springboot_test3/controller/mapstruct/BacklogMapStruct.java
+++ src/main/java/com/han/test/springboot_test3/controller/mapstruct/BacklogMapStruct.java
@@ -3,8 +3,6 @@ package com.han.test.springboot_test3.controller.mapstruct;
 import com.han.test.springboot_test3.domain.Backlog;
 import com.han.test.springboot_test3.domain.dto.BacklogDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mappings;
-import org.mapstruct.factory.Mappers;
 
 @Mapper( componentModel = "spring")
 public interface BacklogMapStruct {
--- src/test/java/com/han/test/springboot_test3/MapStructTest.java
+++ src/test/java/com/han/test/springboot_test3/MapStructTest.java
@@ -4,8 +4,13 @@ import com.han.test.springboot_test3.controller.mapstruct.BacklogMapStruct;
 import com.han.test.springboot_test3.domain.Backlog;
 import com.han.test.springboot_test3.domain.dto.BacklogDto;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MapStructTest {
 
     @Autowired
@@ -18,8 +23,12 @@ public class MapStructTest {
         backlog.setBusinessNumber("123");
         backlog.setLeaveTime(12314543L);
         System.out.println(mapStruct);
+
+
         BacklogDto dto = mapStruct.toDto(backlog);
 //        BacklogDto dto = mapStruct.INSTANCE.toDto(backlog);
+
+
         System.out.println(dto);
     }
 }
